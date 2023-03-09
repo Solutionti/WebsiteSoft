be6787ff3b9bd95548c139ff010d2076b2ee716b
Desarrollador Jerson Galvez <desarrollo.gocho@gmail.com>
2023-03-09 11:13:09 -0500

    agragando los estilos de fontawesome

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,2 +1,5 @@
+<link rel="stylesheet" href="{{ asset('fontawesome/css/fontawesome.min.css') }}">
+<link rel="stylesheet" href="{{ asset('fontawesome/css/solid.min.css') }}">
+<link rel="stylesheet" href="{{ asset('fontawesome/css/regular.min.css') }}">
 <link rel="stylesheet" href="{{ asset('css/galvezj.min.css') }}">
 <link rel="stylesheet" href="{{ asset('css/bgalvezj.min.css') }}">
